f848ad6479c8b38ef6ed5dfc78d61ef73d8f0417
Donovan <donovan@MAC.local>
2021-03-07 09:45:12 +0200

    First commit

---
+++ includes/js/vue.js
@@ -0,0 +1,18 @@
+
+new Vue({
+    el : '#myVueItem',
+    data : {
+        header : 'Vue is good to go!'
+    }
+});
+
+// New Product Vue Class
+let product = new Vue({
+    el : '',
+    data : {
+        tagline : '',
+        category : '',
+        price : '',
+        description : []
+    }
+});
